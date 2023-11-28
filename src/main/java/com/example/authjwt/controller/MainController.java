b8a91421a6e833f41ca9a7f66e1cfe79f6fea615
Emmanuel Mireku Omari <emmanuel.omari@amalitech.com>
2023-11-28 16:36:10 +0000

    files added

---
+++ src/main/java/com/example/authjwt/controller/MainController.java
@@ -0,0 +1,4 @@
+package com.example.authjwt.controller;
+
+public class MainController {
+}
---
+++ src/main/java/com/example/authjwt/model/UserInfo.java
@@ -0,0 +1,42 @@
+package com.example.authjwt.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserInfo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    private String email;
+    private String password;
+    private String roles;
+
+
+//    public String getName() {
+//        return name;
+//    }
+//
+//    public String getPassword() {
+//        return password;
+//    }
+//
+//    public String getRoles() {
+//        return roles;
+//    }
+
+
+
+
+}
